ec0a930a4854fd6724d7657e6f55e302ae83eae2
Yi Hong Lim <limyihong1415@gmail.com>
2026-01-31 13:20:25 -0800

    chore: import packages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // disable Lightning CSS / oxide in Next build
+    optimizeCss: false
+  }
 };
 
 export default nextConfig;
